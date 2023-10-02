715ccf2ba00f22d9b58248b2d67ac30a058c6050
Lucas Santos de Oliveira <lucas.ramuh@gmail.com>
2023-10-01 22:24:33 -0300

    Inclusão do teste da função inactive.

--- estudoDeCamadas/src/main/java/com/estudos/estudoDeCamadas/service/UserService.java
+++ estudoDeCamadas/src/main/java/com/estudos/estudoDeCamadas/service/UserService.java
@@ -5,6 +5,7 @@ import com.estudos.estudoDeCamadas.entities.User;
 import com.estudos.estudoDeCamadas.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -12,6 +13,7 @@ public class UserService {
     @Autowired
     private UserRepository repository;
 
+    @Transactional(readOnly = true) //parâmetro readOnly devido ser uma operação apenas de leitura.
     public UserDTO findById(Long id){
         User entity = repository.findById(id).get();
         return new UserDTO(entity);
